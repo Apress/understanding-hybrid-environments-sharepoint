2eaab0cfb6b3b42175b9b0d32261f0d87a972933
Mark Powers <POWE04@springernature.com>
2020-08-31 16:54:59 -0400

    Initial commit

---
+++ Nachan_Ch03_Configure SharePoint Server 2019/Bulk Import AD Users/BulkImportADUsers.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module ActiveDirectory
+
+$Users = Import-Csv -Delimiter ";" -Path "C:\ADUsers.csv"            
+
+foreach ($User in $Users)            
+{            
+    $Displayname = $User.FirstName + " " + $User.Lastname            
+    $UserFirstname = $User.Firstname            
+    $UserLastname = $User.Lastname            
+    $OU = $User.OU           
+    $SAM = $User.SAM            
+    $UPN = $User.Firstname + $User.Lastname + "@" + $User.MailDomain            
+    $Description = $User.Description            
+    $Password = $User.Password       
+    New-ADUser -Name $Displayname -DisplayName $Displayname -SamAccountName $SAM -UserPrincipalName $UPN -GivenName $UserFirstname -Surname $UserLastname -Description $Description -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true
+    Write-Host $User.SAM "created successfully"
+}
